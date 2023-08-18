5e777ff1e105869866f8fd483ef4bbadc4d45ec7
14failstry <lizzahbrah@gmail.com>
2023-08-18 00:14:41 -0700

    update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /*
- * main -Determie if a number is negative,positive or zero
+ * main -Determine if a number is positive ,negative or zero
  *
  * Return:Always 0(Success)
  */
