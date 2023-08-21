b258b5075931c6e28752fe4f94c288d1b7849aa8
14failstry <lizzahbrah@gmail.com>
2023-08-21 02:32:20 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function that prints  the alphabet in lowercase
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char j;
+	int i;
+
+	i = 0;
+
+	while (i < 0)
+	{j = 'a';
+		while (j <= 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar('\n);
+		i++;
+	}
+}
