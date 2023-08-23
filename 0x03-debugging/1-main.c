343795fc9a9467ee19314d5d9675c08193bf46a9
14failstry <lizzahbrah@gmail.com>
2023-08-23 10:12:03 -0700

    like, comment, subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main-a function that checks for an infinite loop
+*return: 0
+*/
+int mai(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+i = 0;
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
