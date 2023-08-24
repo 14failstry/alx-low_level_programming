d70b22032902d9d07a271545cfe47ccf73a26d7b
14failstry <lizzahbrah@gmail.com>
2023-08-24 04:16:21 -0700

    more umbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,17 +4,16 @@
  * print_numbers- a function that prints numbers 0 to 9
  * followed by a new line
  *
- * Return:void
+ * Return: numbers from 0 to 9
  */
 
 void print_numbers(void)
 {
-	char c;
+	int c;
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(c);
+		_putchar(c + '0');
 	}
-	_putchar('\n');
-}
+	        _putchar('\n');
 
