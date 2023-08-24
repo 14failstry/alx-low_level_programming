6700b2bc65ddd439c1b323c0c76d20108f70e239
14failstry <lizzahbrah@gmail.com>
2023-08-24 03:44:57 -0700

    update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,16 +2,12 @@
 
 /**
  * _isupper - a function that checks for uppercase characters
- * @c: the charater to be checked
- *
- * Return:1 if c is uppercase
- * 0 otherwise
- *
+ * @c: the character to be checked
+ * Return: 1 if c uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-
-	if (c >= 'A' && c <= 'Z')
+	if ((c <= 'A'); &&c <= 'Z')
 		return (1);
 	else
 		return (0);
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,7 +5,7 @@
  * except 2 and 4
  *Return: void
  */
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 		char c;
 
