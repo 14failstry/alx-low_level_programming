df5a1ea0b1fe4f9a173682b0ef34a72e1a5f484f
14failstry <lizzahbrah@gmail.com>
2023-08-25 01:24:51 -0700

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_most_numbers- a function thta prints numbers 0 to 9
@@ -7,18 +8,13 @@
  */
 void print_most_numbers(void)
 {
-int c;
+	char c;
 
-for (c = 48; c < 58; c++)
-{
-	if (c != 50)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (c != 52)
-{
-_putchar(c);
+		if (!(c == '2' || c == '4'))
+			putchar(c);
+	}
+	putchar('\n');
 }
-}
-}
-_putchar('\n');
-
 
