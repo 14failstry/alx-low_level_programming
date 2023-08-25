0f7be9d638074d2e1f1c93383118c32d797ff154
14failstry <lizzahbrah@gmail.com>
2023-08-25 06:08:02 -0700

    update

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,10 @@
- #include <stdio.h>
+#include <stdio.h>
 
-  /**
-  * main- Entry point
-  *
-  * all single digit numbers of base 10 starting from 0
-  *
-  * Return: Always 0(Success)
-  */
+/**
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
